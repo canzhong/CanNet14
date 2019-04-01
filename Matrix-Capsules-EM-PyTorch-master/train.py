aa005c38f69e71d212d391fb0b455dda4218af04
Zhong Can <zhongcan00@gmail.com>
2019-04-01 17:09:19 -0500

    commit

--- Matrix-Capsules-EM-PyTorch-master/train.py
+++ Matrix-Capsules-EM-PyTorch-master/train.py
@@ -293,7 +293,7 @@ class ConvCaps(nn.Module):
         b, h, w, c = x.shape
         if not self.w_shared:
             # add patches
-            x, oh, ow = self.add_pathes(x, self.B, self.K, self.psize, self.stride)
+            #x, oh, ow = self.add_pathes(x, self.B, self.K, self.psize, self.stride)
 
             # transform view
             p_in = x[:, :, :, :, :, :self.B*self.psize].contiguous()
