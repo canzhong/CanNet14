46c024d9b2cf15d8ec9278dc88dfcf39cae48df8
Zhong Can <zhongcan00@gmail.com>
2019-04-02 13:28:36 -0500

    commit

--- Matrix-Capsules-EM-PyTorch-master/train.py
+++ Matrix-Capsules-EM-PyTorch-master/train.py
@@ -618,30 +618,6 @@ class CapsODE(nn.Module): ##ODEFunc(nn.Module)
         return out
 
 
-    def __init__(self, A=32, B=32, C=32, D=32, E=10, K=3, P=4, iters=2):
-        super(CapsNet, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=A,
-                               kernel_size=5, stride=2, padding=2)
-        self.bn1 = nn.BatchNorm2d(num_features=A, eps=0.001,
-                                 momentum=0.1, affine=True)
-        self.relu1 = nn.ReLU(inplace=False)
-        self.primary_caps = PrimaryCaps(A, B, 1, P, stride=1)
-        self.conv_caps1 = ConvCaps(B, C, K, P, stride=2, iters=iters)
-        self.conv_caps2 = ConvCaps(C, D, K, P, stride=1, iters=iters)
-        self.class_caps = ConvCaps(D, E, 1, P, stride=1, iters=iters,
-                                        coor_add=True, w_shared=True)
-        self.nfe = 0
-
-    def forward(self, t, x):
-        self.nfe += 1
-        out = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu1(x)
-        x = self.primary_caps(x)
-        x = self.conv_caps1(x)
-
-        x = self.class_caps(x)
-        return x
 
 class CapsODEBlock(nn.Module):
 
