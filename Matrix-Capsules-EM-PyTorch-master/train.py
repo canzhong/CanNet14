4922af39b3a99deff1e902e4fee195a765231f10
Zhong Can <zhongcan00@gmail.com>
2019-04-02 22:42:42 -0500

    m

--- Matrix-Capsules-EM-PyTorch-master/train.py
+++ Matrix-Capsules-EM-PyTorch-master/train.py
@@ -598,7 +598,7 @@ class CapsODE(nn.Module): ##ODEFunc(nn.Module)
         super().__init__()
         #self.primary_caps = PrimaryCaps(A=dim, B=dim, K=1, P=4, stride=1)
 
-        self.convcaps = ConvCaps(B=dim, C=dim, K=3, stride=1, iters=1, coor_add=False, w_shared=False)#ConcatConvCaps(dim)
+        self.convcaps = ConvCaps(B=dim, C=dim, K=1, stride=2, iters=1, coor_add=False, w_shared=False)#ConcatConvCaps(dim)
         #self.classcaps = ConcatConvCaps(B=dim, C=dim )
         self.nfe = torch.tensor(0)
 
